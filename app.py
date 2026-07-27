30ad0ebd84cd9a6b00a18343dbf36b9d6b9b472e
Aaron Martínez <aaronmtz6613@gmail.com>
2026-07-27 11:28:26 -0500

    Refactor imports and update chain modules

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import chainlit as cl
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_cohere import CohereEmbeddings, ChatCohere
 from langchain_community.vectorstores import FAISS
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 def iniciar_agente():
